5141e058f93f5f5e868a7f7cd8017479a0fb4476
Relvixx <relvixx89@gmail.com>
2026-06-30 14:12:59 +0530

    fix: include config.js for Supabase/EmailJS client-side keys

---
+++ config.js
@@ -0,0 +1,8 @@
+window.CONFIG = {
+  SUPABASE_URL: 'https://hubxctffnforizfayfzv.supabase.co',
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1YnhjdGZmbmZvcml6ZmF5Znp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMTY1MzksImV4cCI6MjA5Nzg5MjUzOX0.qn7eHCjefFw9tDydCA8Xpn88_YR84WsetU04DItAKXU',
+  EMAILJS_PUBLIC_KEY: 'ONjViRAkMpCeXS4_U',
+  EMAILJS_SERVICE_ID: 'service_xb39y8x',
+  EMAILJS_TEMPLATE_ADMIN: 'template_5sovydg',
+  EMAILJS_TEMPLATE_USER: 'template_om2uizy'
+};
